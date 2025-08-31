d6f5d0e0d26ecc88ae3656b16961a692d56ed42b
Viktor Morin <viktor.vintertass@sourcein.se>
2025-08-31 17:30:34 +0200

    updated project

--- react/next.config.ts
+++ react/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const path = require('path');
+
 const nextConfig: NextConfig = {
+  distDir: path.join(__dirname, '..', '.next') // outputs one folder up
   output: "export"
 };
 

53228de17489737122b46404e4eb512691603323
Viktor Morin <viktor.vintertass@sourcein.se>
2025-08-31 18:26:27 +0200

    working react

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "sourcein"; 
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
+  //output: "export"
 };
 
 export default nextConfig;
